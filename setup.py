78420dd18cdc4430f8b6ea7cb8ea29878c034b56
Abhishek Pratap <pratapsinghabhishek112@gmail.com>
2023-03-02 13:47:45 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="pratap",
+    author_email="pratapsinghabhishek112@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
