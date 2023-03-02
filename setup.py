d5796095896bcda64ca94a2419362db72288d149
Abhishek Pratap <pratapsinghabhishek112@gmail.com>
2023-03-02 14:04:25 +0530

    third commit

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import find_packages,setup
+from typing import List
 
-def get_requirements():
-    pass
+require_file_name="requirements.txt"
+Hypen_e_dot="-e ."
+
+def get_requirements()->List[str]:
+    with open(require_file_name) as file_require:
+        requirement_list=file_require.readlines()
+    requirement_list=[requirement_name.replace("\n","") for requirement_name in requirement_list]
+    if Hypen_e_dot in requirement_list:
+        requirement_list.remove(Hypen_e_dot)
+    return requirement_list
+
 
 setup(
     name="sensor",
